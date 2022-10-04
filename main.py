998e4f56c9b5964ee33a818f4fb3b8770a80dae9
TruongHuy-TheAstronaut <TruongHuy012345@gmail.com>
2022-10-04 16:25:47 +0700

    Initial workable version, need to be refactored

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ print("Hello The Brave New World")
 
 import sys
 from Adafruit_IO import MQTTClient
-from simple_ai import *
+# from simple_ai import *
+from physical import *
 import time
 
 AIO_FEED_ID = ["sensor_1", "sensor_2", "sensor_3", "actuator_1", "actuator_2", "vision_detection"]
 AIO_USERNAME = "truonghuy"
-AIO_KEY = ""
+AIO_KEY = "aio_PXVN21tAWJcKoCCthvhVAnu7mish"
 
 def connected(client):
     print("Ket noi thanh cong ...")
@@ -23,6 +24,12 @@ def disconnected(client):
 
 def message(client , feed_id , payload):
     print("Nhan du lieu" + feed_id + " : "+ payload)
+    state = True if (str(payload) == "1") else False
+    # let state = (str(payload) == "1") ? True : False
+    if feed_id == "actuator_1":
+        setDevice1(state)
+    if feed_id == "actuator_2":
+        setDevice2(state)
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -31,9 +38,20 @@ client.on_message = message
 client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
+#
+# def set_state(client, feed_id, payload):
+#     state = (str(payload) == "1") ? True: False
+#     if feed_id == "actuator_1":
+#         setDevice1(state)
+#     if feed_id == "actuator_2":
+#         setDevice2(state)
 
 while True:
-    time.sleep(5)
-    image_capture()
-    ai_result = image_detector()
-    client.publish("vision_detection", ai_result)
+    pass
+
+
+# while True:
+#     time.sleep(5)
+#     image_capture()
+#     ai_result = image_detector()
+#     client.publish("vision_detection", ai_result)
